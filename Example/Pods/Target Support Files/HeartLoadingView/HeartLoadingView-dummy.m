5cc52f57cd6a86478cf9d3c33a455adf9d96e984
Dima Shvets <aoedima@gmail.com>
2025-08-11 15:34:10 +0300

    chore: remove CocoaPods support

--- Example/Pods/Target Support Files/HeartLoadingView/HeartLoadingView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HeartLoadingView : NSObject
-@end
-@implementation PodsDummy_HeartLoadingView
-@end
--- Example/Pods/Target Support Files/Pods-HeartLoadingView_Tests/Pods-HeartLoadingView_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HeartLoadingView_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_HeartLoadingView_Tests
-@end
